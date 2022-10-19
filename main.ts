89a81a025b5ed0c04a70a037d3cd260bcae411f5
2E16 <115064797+2E16@users.noreply.github.com>
2022-10-19 15:01:22 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,45 +1,37 @@
 let x = 0
-let y = 0
+let choice = 0
 input.onButtonPressed(Button.A, function () {
 	
-})
-input.onButtonPressed(Button.AB, function () {
-    x += 1
-    basic.clearScreen()
-    led.plot(x, y)
-    basic.pause(200)
-    if (x >= 4) {
-        x += -1
-    }
-    if (x <= 0) {
-        x += 1
-    }
 })
 input.onButtonPressed(Button.B, function () {
 	
 })
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        x += 1
-        basic.clearScreen()
-        led.plot(x, y)
-        basic.pause(200)
-    }
-    if (x >= 4) {
-        x = -1
+    if (input.buttonIsPressed(Button.AB)) {
+        let y = 0
+        if (x >= 4) {
+            choice = 0
+        }
+        if (x <= 0) {
+            choice = 1
+        }
+        if (choice == 0) {
+            x += -1
+            basic.clearScreen()
+            led.plot(x, y)
+            basic.pause(200)
+        }
+        if (choice == 1) {
+            x += 1
+            basic.clearScreen()
+            led.plot(x, y)
+            basic.pause(200)
+        }
     }
 })
 basic.forever(function () {
 	
 })
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.B)) {
-        y += 1
-        basic.clearScreen()
-        led.plot(x, y)
-        basic.pause(200)
-    }
-    if (y >= 4) {
-        y = -1
-    }
+	
 })
